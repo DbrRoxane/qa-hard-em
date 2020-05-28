e930971ca05e2a51e4a7ddd862e620fb567e15b3
DbrRoxane <roxane.debruyker.1@ulaval.ca>
2020-05-28 13:37:44 -0400

    launch with better data

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ data=$1
 loss=$2
 bs=16
 pbs=256
-output_dir="../data/output/min_trainsum_hardem_r5"
+output_dir="../data/output/min_trainsum_hardem_r6"
 data_dir="../data/narrativeqa/min_format"
 
 if [ ${loss} = "hard-em" ]
@@ -21,11 +21,11 @@ fi
 #done
 #dev_file="${data_dir}/${data}-dev.json"
 #test_file="${data_dir}/${data}-test.json"
-train_file="${data_dir}/min_sums_with_answer_train.json_r5"
-dev_file_all="${data_dir}/min_all_with_answer_dev.json_allrankingtech_severalanswers_r5"
-test_file_all="${data_dir}/min_all_with_answer_test.json_allrankingtech_severalanswers_r5"
-dev_file_sum="${data_dir}/min_sums_with_answer_dev.json_r5"
-test_file_sum="${data_dir}/min_sums_with_answer_dev.json_r5"
+train_file="${data_dir}/min_sums_with_train.json_r6"
+dev_file_all="${data_dir}/min_all_with_answer_dev.json_allrankingtech_severalanswers_r5_28mai"
+test_file_all="${data_dir}/min_all_with_answer_test.json_allrankingtech_severalanswers_r5_28mai"
+dev_file_sum="${data_dir}/min_sums_dev.json_r6"
+test_file_sum="${data_dir}/min_sums_test.json_r6"
 
 
 #min_sums_with_answer_test.json_r5"
@@ -41,33 +41,32 @@ test_file_sum="${data_dir}/min_sums_with_answer_dev.json_r5"
 #sum_test_file="${data_dir}/min_summaries_without_answer_test.json"
 #sum_dev_file="${data_dir}/min_summaries_without_answer_dev.json"
 
-#python3 main.py --do_train --output_dir ${output_dir} \
-#          --train_file ${train_file} --predict_file ${dev_file} \
-#          --train_batch_size ${bs} --predict_batch_size ${pbs} --verbose --loss_type ${loss} --tau ${tau} --verbose_logging 
+python3 main.py --do_train --output_dir ${output_dir} \
+          --train_file ${train_file} --predict_file ${dev_file} \
+          --train_batch_size ${bs} --predict_batch_size ${pbs} --verbose --loss_type ${loss} --tau ${tau} --verbose_logging 
 #
 
 python3 main.py --do_predict --output_dir ${output_dir} \
-          --predict_file ${test_file_all} \
+         --predict_file ${test_file_sum} \
 	  --init_checkpoint "${output_dir}/best-model.pt"\
-          --predict_batch_size ${pbs} --n_paragraphs "1,2,3,5,7,10" --prefix min_predall_oracle_r5_allranking_trainedsum_test
-
-python3 main.py --do_predict --output_dir ${output_dir} \
-          --predict_file ${dev_file_all} \
-	  --init_checkpoint "${output_dir}/best-model.pt" \
-          --predict_batch_size ${pbs} --n_paragraphs "1,2,3,5,7,10" --prefix min_predall_oracle_r5_allranking_trainedsum_dev
-
-python3 main.py --do_predict --output_dir ${output_dir} \
-          --predict_file ${test_file_sum} \
-	  --init_checkpoint "${output_dir}/best-model.pt"\
-          --predict_batch_size ${pbs} --n_paragraphs "1" --prefix min_predsum_r5_trainedsum_test
+          --predict_batch_size ${pbs} --n_paragraphs "1" --prefix min_predsum_r6_test
 
 python3 main.py --do_predict --output_dir ${output_dir} \
           --predict_file ${dev_file_sum} \
 	  --init_checkpoint "${output_dir}/best-model.pt" \
-          --predict_batch_size ${pbs} --n_paragraphs "1" --prefix min_predsum_r5_trainedsum_dev
+          --predict_batch_size ${pbs} --n_paragraphs "1" --prefix min_predsum_r6_dev
 
+python3 main.py --do_predict --output_dir ${output_dir} \
+          --predict_file ${test_file_all} \
+	  --init_checkpoint "${output_dir}/best-model.pt"\
+          --predict_batch_size ${pbs} --n_paragraphs "1,2,3,5,7,10" --prefix min_predall_oracle_r6_allranking_test
 
-
+python3 main.py --do_predict --output_dir ${output_dir} \
+          --predict_file ${dev_file_all} \
+	  --init_checkpoint "${output_dir}/best-model.pt" \
+          --predict_batch_size ${pbs} --n_paragraphs "1,2,3,5,7,10" --prefix min_predall_oracle_r5_allranking_dev
+#
+#
 #python3 /app/trivia-qa/main.py --do_predict --output_dir ${output_dir} \
 #          --predict_file ${all_without_test_file} \
 #	  --init_checkpoint "/nas-data/trivia-qa/outputs/trivia-qa-job-0.1.dev53-ga79f2d5/narrativeqa-first-only_14mai/best-model.pt"\
